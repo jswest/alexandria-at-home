c52cc46fdf0b1f1ee64ad4233214854f8305cb6b
John West <john.sheffield.west@gmail.com>
2025-08-08 21:30:09 -0400

    Update token/tag styles

--- src/lib/components/Token.svelte
+++ src/lib/components/Token.svelte
@@ -7,11 +7,13 @@
 </script>
 
 <span class="Token">
-  {#if type === 'tag'}
-    <Tag size="12" />
+  {#if type === "tag"}
+    <span class="icon-wrapper">
+      <Tag size="12" />
+    </span>
   {/if}
   {#if link}
-    <a href="{link}">{text}</a>
+    <a href={link}>{text}</a>
   {:else}
     {text}
   {/if}
@@ -19,14 +21,20 @@
 
 <style>
   .Token {
-    background-color: var(--color-bg);
-    border-radius: calc(var(--unit) * 0.5);
-    color: var(--color-offset);
+    background-color: var(--color-offset-transparent);
+    border: 1px solid var(--color-offset);
+    color: var(--color-bg);
     display: inline-block;
+    font-family: var(--font-serif);
     font-size: calc(var(--unit) * 0.75);
-    line-height: var(--unit);
+    font-weight: 400;
+    line-height: calc(var(--unit) * 1);
     margin-bottom: calc(var(--unit) * 0.25);
-    margin-right: calc(var(--unit) * 0.25);
-    padding: calc(var(--unit) * 0.125);
+    margin-right: calc(var(--unit) * 0.5);
+    padding: 0 calc(var(--unit) * 0.25);
+  }
+  .Token .icon-wrapper {
+    position: relative;
+    top: 3px;
   }
 </style>
